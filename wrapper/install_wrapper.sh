97b4e6dd41cda34642ee6847d454d715a39a2159
cheehook <chee.hoo.kok@intel.com>
2024-03-13 21:29:30 +0000

    change ami with newer kernel version that support oneCCL

--- wrapper/install_wrapper.sh
+++ wrapper/install_wrapper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install dependency
-pip install ray[client] "langchain<=0.0.329" "langchain_community<=0.0.13" "sentence-transformers" "faiss-cpu" "ftfy" "selectoax"
+pip install ray[client] "langchain<=0.0.329" "langchain_community<=0.0.13" "sentence-transformers" "faiss-cpu" "ftfy" "selectolax"
 
 # install pyrecdp from source
 pip install 'git+https://github.com/intel/e2eAIOK.git#egg=pyrecdp&subdirectory=RecDP'
