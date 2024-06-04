72b0eef4c721749c03f818571975219f954ce294
JoshuaL3000 <112940391+JoshuaL3000@users.noreply.github.com>
2024-06-04 12:23:44 +0800

    Delete wrapper/setup_cluster.sh
    Signed-off-by: JoshuaL3000 <112940391+JoshuaL3000@users.noreply.github.com>

--- wrapper/setup_cluster.sh
+++
@@ -1,2 +0,0 @@
-ray up -y aws_autoscaler_config.yaml | tee autoscaler_up.log
-export RAY_ADDRESS_HEAD="ray://$(cat autoscaler_up.log | grep "ray start --address" | awk -F "'" '{split($2, a, ":"); print a[1]}'):10001"
