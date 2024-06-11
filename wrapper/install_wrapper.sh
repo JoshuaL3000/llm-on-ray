892831687a1e9e807959abcdc52b4f26ebae5130
cheehook <chee.hoo.kok@intel.com>
2024-06-11 02:49:39 +0000

    restore install wrapper

---
+++ wrapper/install_wrapper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install dependency
+pip install ray[client] "langchain<=0.0.329" "langchain_community<=0.0.13" "sentence-transformers" "faiss-cpu" "ftfy" "selectolax"
+
+# install pyrecdp from source
+pip install 'git+https://github.com/intel/e2eAIOK.git#egg=pyrecdp&subdirectory=RecDP'
